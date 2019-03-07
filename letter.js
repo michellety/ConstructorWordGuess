83dcf0de35e24f7c9e54e3b607aa7496442621f7
michelle <michelletyson29@gmail.com>
2019-03-07 11:01:43 -0800

    initial instructions

---
+++ letter.js
@@ -0,0 +1,8 @@
+/// requries no other files
+/// make a constructor called Letter
+/// display blank placeholder 
+///define:
+    ///a string value to store the undelying character for the letter
+    ///boolean whether the letter has been guessed
+    ///function that returns the letter guessed or placeholder if not guessed
+    //function that checks the letter(as an arg) and updating the stored boolean if correctly guessed
---
+++ word.js
@@ -0,0 +1,10 @@
+///constructor Word that depends on the Letter constructor
+///requires the file Letter.js
+//creates an object representing the current word the user is guessing
+///defines:
+    ///an array of new Letter objects representing the letters in the word
+    ///function that returns a string of the word,
+        ///should call the function on each letter object defined in letter.js 
+        //that displays the character or underscore and concatentate those together
+    ///function that takes a character as an argument and calls the guess function on each letter
+        //the second funciton defined in letter.js
